0bc8e902c3c15923c2474b9022fd90f26431dab4
anamariaagudelo <anamariaagudeloisi@gmail.com>
2019-03-27 15:47:16 -0500

    Configuration App estructure OK

---
+++ src/components/RegisterCandidate/RegisterCandidate.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+
+class RegisterCandidate extends React.Component {
+    render(){
+        return(
+            <Aux>
+                <div>Cookies</div>
+                <div>Form</div>
+            </Aux>
+        )
+    }
+
+}
+
+export default RegisterCandidate;
---
+++ src/components/SearchGithubUser/SearchGithubUser.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+
+class SearchGithubUser extends React.Component {
+    render(){
+        return(
+            <Aux>
+                <div>Search Input</div>
+                <div>Table</div>
+            </Aux>
+        )
+    }
+
+}
+
+export default SearchGithubUser;
